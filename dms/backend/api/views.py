f8d7b5dc8c9d0c491c259e9180dc0dbdf4a2f55f
Mostafa-0mda <mostafa_sayed93@outlook.com>
2023-09-16 19:20:59 +0300

    16/09/2023

--- dms/backend/api/views.py
+++ dms/backend/api/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+from products.models import product
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-
-  body = request.body
+  model_data = product.objects.all().order_by("?").first()
   data = {}
-  try:
-    data =json.loads(body)
-  except:
-    pass
-  print(data)
-
+  if model_data:
+    data['id'] = model_data.id
+    data['title'] = model_data.title
+    data['content'] = model_data.content
+    data['price'] = model_data.price
   return JsonResponse(data)
