dd0d334773ff28892fa215bfdf1190e133a9ca92
Mostafa Emad <mostafaemad@systemtech.loc>
2023-09-16 14:23:14 +0300

    16/09

--- dms/py_client/basic.py
+++ dms/py_client/basic.py
@@ -1,3 +1,7 @@
 import  requests
 
-endpoint = "https://www.github.com"
+#endpoint = "https://httpbin.org/statis/200/"
+endpoint = "http://localhost:8000/"
+
+get_response = requests.get(endpoint, json={"query":"Hello world"})
+print(get_response.text)
